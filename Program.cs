ab0dec236bb00e8f512ef4860671604fc6d3d7ad
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 <muhammadjonmuydinjanov03@gmail.com>
2021-10-07 15:23:47 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var lab2 = new Lab2();
+           lab2.Problem1();
+
+           
+        }
+    }
+}
